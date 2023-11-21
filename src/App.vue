d05b82fa780b7768e93bf5c6f88948c77feeae6a
lipipereira <filipicostasilva@gmail.com>
2023-11-21 17:21:21 -0300

    Implemented card to our dashboard

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,68 @@
 
     <v-main>
       <v-container>
+        <h1>Dashboard</h1>
         <v-row>
-          <v-col cols="1"></v-col>
+          <v-col cols="12" md="4" lg="3">
+            <v-card flat class="border">
+              <v-img
+                class="align-end text-white"
+                src="https://images.pexels.com/photos/533630/pexels-photo-533630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              >
+                <v-card-title>Top 10 lugares para visita</v-card-title>
+              </v-img>
+              <v-card-subtitle class="pt-3">Them de bordo</v-card-subtitle>
+              <v-card-text>
+                <div>Whitehaven Beach</div>
+
+                <div>Whitsunday Island, Whitsunday Islands</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="4" lg="3">
+            <v-card flat class="border">
+              <v-img
+                class="align-end text-white"
+                src="https://images.pexels.com/photos/533630/pexels-photo-533630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              >
+                <v-card-title>Top 10 lugares para visita</v-card-title>
+              </v-img>
+              <v-card-subtitle class="pt-3">Them de bordo</v-card-subtitle>
+              <v-card-text>
+                <div>Whitehaven Beach</div>
+
+                <div>Whitsunday Island, Whitsunday Islands</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="4" lg="3">
+            <v-card flat class="border">
+              <v-img
+                class="align-end text-white"
+                src="https://images.pexels.com/photos/533630/pexels-photo-533630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              >
+                <v-card-title>Top 10 lugares para visita</v-card-title>
+              </v-img>
+              <v-card-subtitle class="pt-3">Them de bordo</v-card-subtitle>
+              <v-card-text>
+                <div>Whitehaven Beach</div>
+
+                <div>Whitsunday Island, Whitsunday Islands</div>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn>Ver mais</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+
         </v-row>
       </v-container>
     </v-main>
