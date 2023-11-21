02bdb18abc71aa0471b3990b472f7718407b8d57
lipipereira <filipicostasilva@gmail.com>
2023-11-21 16:57:44 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-app-bar flat class="border-b">
+      <v-app-bar-title>Curso de Vuetify</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="1"></v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup></script>
